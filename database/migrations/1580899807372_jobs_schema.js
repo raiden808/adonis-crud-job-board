8460718e0f8d66b24009737720464a7b2816d51b
raiden808 <raiden.nalasa@gmail.com>
2020-02-05 19:09:51 +0800

    added model for jobs

--- database/migrations/1580899807372_jobs_schema.js
+++ database/migrations/1580899807372_jobs_schema.js
@@ -3,10 +3,25 @@
 /** @type {import('@adonisjs/lucid/src/Schema')} */
 const Schema = use('Schema')
 
+/**
+ * Database schema created by
+ * cli - adonis make:migration jobs
+ * 
+ * By runnin 'adonis migration:run'
+ * Schema will be created in mysql
+ * 
+ * Additional note:
+ * To check if schema exist check your 
+ * adonis_schema table in mysql
+ */
 class JobsSchema extends Schema {
   up () {
     this.create('jobs', (table) => {
       table.increments()
+      table.string('title')
+      table.string('link')
+      table.string('description')
+      table.string('user_id')
       table.timestamps()
     })
   }
